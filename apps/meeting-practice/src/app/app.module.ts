aca891d636cb088e4dc6aec14ed9a78d8b777946
Adept-Rohan <rohan1d20@gmail.com>
2023-11-30 10:54:26 +0545

    integrated graphql in app module

--- apps/meeting-practice/src/app/app.module.ts
+++ apps/meeting-practice/src/app/app.module.ts
@@ -1,6 +1,23 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UserModule } from '@org4/user';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [UserModule],
+      useFactory: () => ({
+        playground: false,
+        sortSchema: true,
+        introspection: true,
+        autoSchemaFile: join(__dirname, 'schema.gql'),
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      }),
+    }),
+  ],
 })
 export class AppModule {}
